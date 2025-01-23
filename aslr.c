ae0dd4caea3695593824a34b53f7e5fa396d1c25
nowan02 <starkgeri@gmail.com>
2025-01-23 12:40:08 +0100

    sósz

---
+++ aslr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main()
+{int one_var = 5;
+int two_var = 11;
+
+char* string = "Szöveg";
+    printf("%s %d + %d = %d", string, one_var, two_var, one_var+two_var);
+
+    return 0;
+}
